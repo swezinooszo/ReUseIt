b6643138923a44a5fab2f588b755d4a1fc4d35f3
swezinooszo <swezinoo410@gmail.com>
2025-08-10 11:50:55 -0600

    Delete frontend/config/config.js

--- frontend/config/config.js
+++
@@ -1,4 +0,0 @@
-// You can define constants here
-export const IP_ADDRESS = '192.168.0.10'//'142.3.66.244'//////
-export const API_BASE_URL = `http://${IP_ADDRESS}:8000/api`;
-export const MAP_API_KEY = 'AIzaSyCqBU9PK7AtEQG6ffI3TRLzCdhPHjrtey8'
